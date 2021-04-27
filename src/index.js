414badec6296bd6fe50e0e92d589f46952c9cb0f
aezakmi007 <abdullahfarhan910@gmail.com>
2021-04-27 14:25:38 +0530

    Initial Commit Setting Up the Env.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
+
   document.getElementById('root')
 );
 
