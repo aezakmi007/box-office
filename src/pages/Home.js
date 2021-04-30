778d9350a5f9f64d81cabc299b8fbbd40c25db12
aezakmi007 <abdullahfarhan910@gmail.com>
2021-04-30 11:17:39 +0530

    installed styled components and created styles for search cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../Components/MainPageLayout';
 import apiGet from '../misc/config';
+import ShowGrid from '../Components/Show/ShowGrid';
+import ActorGrid from '../Components/Actor/ActorGrid';
 
 function Home() {
   const [input, setInput] = useState('');
@@ -30,11 +32,11 @@ function Home() {
       return <div>No Results</div>;
     }
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
