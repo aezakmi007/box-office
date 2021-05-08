03506734b3b1afc50398083dc52ec171379c881e
aezakmi007 <abdullahfarhan910@gmail.com>
2021-05-08 09:07:27 +0530

    resolved styling bug

--- src/Components/Show/ShowCard.js
+++ src/Components/Show/ShowCard.js
@@ -10,7 +10,7 @@ function ShowCard({ id, image, name, summary, onStarClick, isStarred }) {
 
   return (
     <StyledShowCard>
-      <div className="img -wrapper">
+      <div className="img-wrapper">
         <img src={image} alt="show" />
       </div>
 
